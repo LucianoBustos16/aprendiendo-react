5868f4c1bdb7c386067c8aa404be803f2cc630a3
Luciano Bustos <bustosluciano89@gmail.com>
2024-11-17 12:01:21 -0300

    First and second projects

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'midudev',
+    name: 'Miguel Ángel Durán',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo H.',
+    isFollowing: false
+  }
+]
+
+export function App () {
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key="Luchito"
+            name="Luciano"
+            userName="Luchito"
+            initialIsFollowing={false}
+          >
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
+}
