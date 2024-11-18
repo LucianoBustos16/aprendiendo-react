68866e250a846d09028296165626008b6995125a
Luciano Bustos <bustosluciano89@gmail.com>
2024-11-18 19:20:58 -0300

    3er project

---
+++ projects/03-mouse-follower/src/App.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react"
+
+function App() {
+
+  const [enabled, setEnabled] = useState(false)
+  const [position, seTPosition] = useState({x: 0, y: 0})
+
+  useEffect(() => {
+    console.log('Se cambio el estado a ', {enabled})
+    
+    const handleMove =  (event) => {
+      const { clientY, clientX} = event
+      console.log('handleMove', { clientY, clientX} )
+      seTPosition({x: clientX, y: clientY})
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
+
+  return (
+    <main>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: '#09f',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}
+      ></div>
+    <button onClick={() => setEnabled(!enabled)}>{enabled ? 'Desactivar' : 'Activar'} seguir puntero</button>
+    </main>
+  )
+}
+
+export default App
