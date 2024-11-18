9767958f0c9d3d586260de503013f351f7a808dc
Luciano Bustos <bustosluciano89@gmail.com>
2024-11-18 16:37:36 -0300

    Finish Project 01

--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,38 +1,43 @@
-// REVISAR
+import { useState } from "react"
+
+export function TwitterFollowCard ({userName = 'unknown', name = 'unknown', initialIsFollowing}) {
+
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+
+    //useState
+    // const [valor, formaDeActualizarlo] = useState(valorInicial)
 
 
+    const text = isFollowing ? 'Seguiendo' : 'Seguir'
+    const buttonClassName = isFollowing
+        ? 'tw-followCard-button is-following'
+        : 'tw-followCard-button'
 
-    console.log("name")
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
 
-//   const text = isFollowing ? 'Siguiendo' : 'Seguir'
-//   const buttonClassName = isFollowing
-//     ? 'tw-followCard-button is-following'
-//     : 'tw-followCard-button'
+    console.log(isFollowing)
 
-//   const handleClick = () => {
-//     setIsFollowing(!isFollowing)
-//   }
-
-//   return (
-//     <article className='tw-followCard'>
-//       <header className='tw-followCard-header'>
-//         <img
-//           className='tw-followCard-avatar'
-//           alt='El avatar de midudev'
-//           src={`https://unavatar.io/${userName}`}
-//         />
-//         <div className='tw-followCard-info'>
-//           <strong>{name}</strong>
-//           <span className='tw-followCard-infoUserName'>@{userName}</span>
-//         </div>
-//       </header>
-
-//       <aside>
-//         <button className={buttonClassName} onClick={handleClick}>
-//           <span className='tw-followCard-text'>{text}</span>
-//           <span className='tw-followCard-stopFollow'>Dejar de seguir</span>
-//         </button>
-//       </aside>
-//     </article>
-//   )
+    return (
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img
+                    className='tw-followCard-avatar'
+                    src={`https://unavatar.io/x/${userName}`}
+                    alt="El avatar de Luchito" />
+                <div className='tw-followCard-info'>
+                    <strong>{name}</strong>
+                    <span className='tw-followCard-info-userName'>@{userName}</span>
+                </div>
+            </header>
+            <aside>
+                <button className={buttonClassName} onClick={handleClick}>
+                <span className="tw-followCard-textFollowing">{text}</span>
+                <span className="tw-followCard-stopFollowing">Dejar de seguir</span>
+                    
+                </button>
+            </aside>
+        </article>
+    )
 }
