2ec8d5f56b1a043ed081eee688450cb4f41c5cf0
Luciano Bustos <bustosluciano89@gmail.com>
2024-11-23 11:21:08 -0300

    Ajustando cositas

--- projects/react-prueba-tecnica/src/App.jsx
+++ projects/react-prueba-tecnica/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 import './App.css'
+import { getRamdomsFacts } from "./services/facts"
 
     const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red&json=true`
@@ -9,13 +10,9 @@ export function App() {
     const [fact, setFact] = useState()
     const [imageUrl, setImageURL] = useState()
 
+
     useEffect(() => {
-        fetch(CAT_ENDPOINT_RANDOM_FACT)
-            .then(res => res.json())
-            .then(data => {
-                const { fact } = data
-                setFact(fact)                
-            })
+        getRamdomsFacts().then(newFact => setFact(newFact))
     }, [])
 
     useEffect(() => {
@@ -23,16 +20,24 @@ export function App() {
         const firstWord = fact.split(' ', 3).join(' ')
                 console.log(firstWord)
 
-                fetch(`https://cataas.com/cat/says/${firstWord}`)
+                fetch(`https://cataas.com/cat/says/${firstWord}?fontSize=64&fontColor=white`)
                 .then( response => {
                     const { url } = response
                     setImageURL(url)}
                 )
     }, [fact])
 
+    const handleClick = async () => {
+       const newFact = await getRamdomsFacts()
+       setFact(newFact)
+    }
+
     return (
         <main>
             <h1>App de gatitos</h1>
+
+            <button onClick={handleClick}>Get new fact</button>
+
             <section>
             {fact && <p>{fact}</p>}
             {imageUrl &&
